c1c711f2914a6b624b1048705bf3de66f7a9d888
Robert Schroeder <robschroeder333@users.noreply.github.com>
2017-09-29 15:27:16 -0400

    feat: read uploaded file

--- renderer.js
+++ renderer.js
@@ -1,3 +1,36 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const fs = require('fs');
+
+
+const form = document.createElement('form');
+const upload = document.createElement('input');
+upload.type = "file";
+upload.id = "upload";
+form.appendChild(upload);
+document.body.appendChild(form);
+
+const reFormatData = dataString => {
+  // do formatting
+}
+
+function getUploadedFile(evt) {
+  var uploadedFile = evt.target.files[0];
+
+  fs.readFile(uploadedFile.path, 'utf-8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+
+
+    fs.writeFile('message.txt', reFormatData(data), (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+
+    });
+
+  });
+}
+
+document.getElementById('upload').addEventListener('change', getUploadedFile, false);
